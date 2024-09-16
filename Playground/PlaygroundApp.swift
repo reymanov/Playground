e40a5f52d8f5c1afca3e92f40bdcedf4fa90fe79
Jakub Rejmann <jakubrejmann@gmail.com>
2024-09-16 23:48:55 +0200

    initialize repo

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Kuba Rejmann on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
